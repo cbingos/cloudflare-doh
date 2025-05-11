6d6e38c325198f3e75d2bd8e3b4fc1bda7f24cd8
cbingos <gameof_life@qq.com>
2025-05-11 21:26:15 +0800

    Update _worker.js
    add quad9

--- _worker.js
+++ _worker.js
@@ -18,6 +18,12 @@ const DEFAULT_PATH_MAPPINGS = {
 			'/query-dns': '/dns-query',
 		},
 	},
+	"/quad9": {
+          "targetDomain": "dns.quad9.net",
+          "pathMapping": {
+                "/query-dns": "/dns-query"
+          }
+        },
 	// Add more path mappings as needed
 };
 
